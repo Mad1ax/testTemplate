0b82ffb32056862979b66a584ede44235010c5f6
Mad1ax <virus_@inbox.ru>
2023-11-25 23:31:37 +0700

    v 1.1

--- app.js
+++ app.js
@@ -1,66 +1,82 @@
-const upBtn = document.querySelector(".up-button");
-const downBtn = document.querySelector(".down-button");
-const sidebar = document.querySelector(".sidebar");
-const container = document.querySelector(".container");
-const mainSlide = document.querySelector(".main-slide");
-const slidesCount = mainSlide.querySelectorAll("div").length;
+const downBtn = document.querySelector('.down-button');
+const upBtn = document.querySelector('.up-button');
+const sidebar = document.querySelector('.sidebar');
+const container = document.querySelector('.container');
+const mainSlide = document.querySelector('.main-slide');
+const slidesCount = mainSlide.querySelectorAll('div').length;
 
+//buttons and wheel for desktop
+
+upBtn.addEventListener('click', () => changeSlide('up'));
+
+downBtn.addEventListener('click', () => changeSlide('down'));
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowUp') {
+    changeSlide('up');
+  } else if (event.key === 'ArrowDown') {
+    changeSlide('down');
+  }
+});
+
+document.addEventListener('wheel', (event) => {
+  if (event.wheelDeltaY === 120) {
+    changeSlide('up');
+  } else if (event.wheelDeltaY === -120) {
+    changeSlide('down');
+  }
+});
+
+//swipe for mobile
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+let y1 = null;
+
+function handleTouchStart(event) {
+  y1 = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+  if (!y1) {
+    return false;
+  }
+  let y2 = event.touches[0].clientY;
+  let yDiff = y2 - y1;
+
+  if (event.touches[0].clientX > sidebar.clientWidth) {
+    if (yDiff < -5) {
+      changeSlide('up');
+    } else if (yDiff > 5) {
+      changeSlide('down');
+    }
+  } else {
+    if (yDiff < -5) {
+      changeSlide('down');
+    } else if (yDiff > 5) {
+      changeSlide('up');
+    }
+  }
+  y1 = null;
+}
+
+//function change slide
 let activeSlideIndex = 0;
 
 sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
 
-upBtn.addEventListener("click", () => {
-  changeSlide("up");
-});
-
-downBtn.addEventListener("click", () => {
-  changeSlide("down");
-});
-
-document.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowUp") {
-    changeSlide("up");
-  } else if (event.key === "ArrowDown") {
-    changeSlide("down");
-  }
-});
-
-let mainLogo = document.querySelector('#mainLogo')
-mainLogo.addEventListener("click", (event) => {
-  event.preventDefault();
-  console.log("logo_clicked");
-});
-
-// window.addEventListener('scroll', e => {
-//     window.scrollTo({top: 0})
-//     console.log('scrollo')
-//   })
-
-document.addEventListener("wheel", (event) => {
-    event.preventDefault()
-  if (event.wheelDeltaY === 120) {
-    changeSlide("up");
-  }
-  if (event.wheelDeltaY === -120) {
-    changeSlide("down");
-  }
-  event.wheelDeltaY === 120
-    ? console.log("scrollUp")
-    : console.log("scrollDown");
-  console.log(event.wheelDeltaY);
-});
-
-function changeSlide(direction) {
-  if (direction === "up") {
+const changeSlide = (direction) => {
+  if (direction === 'up' && activeSlideIndex < slidesCount - 1) {
     activeSlideIndex++;
-    if (activeSlideIndex === slidesCount) {
-      activeSlideIndex = 0;
-    }
-  } else if (direction === "down") {
+    // if (activeSlideIndex === slidesCount) {
+    //   activeSlideIndex = 0;
+    // }
+  } else if (direction === 'down' && activeSlideIndex > 0) {
     activeSlideIndex--;
-    if (activeSlideIndex < 0) {
-      activeSlideIndex = slidesCount - 1;
-    }
+    // if (activeSlideIndex < 0) {
+    //   activeSlideIndex = slidesCount - 1;
+    // }
   }
 
   const height = container.clientHeight;
@@ -68,39 +84,130 @@ function changeSlide(direction) {
   mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
 
   sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
-}
+  console.log(activeSlideIndex);
+};
 
-let buttonAbout = document.querySelector("#buttonAbout");
-buttonAbout.addEventListener("click", (event) => {
-  event.preventDefault();
-  console.log("click1");
-  changeSlideByNav(3);
+const allNavMenuButtons = document.querySelectorAll('.menu__link');
+
+const activeBtnClassAdder = (e) => {
+  allNavMenuButtons.forEach((btn) => btn.classList.remove('menu__link_active'));
+  e.classList.add('menu__link_active')
+};
+
+allNavMenuButtons.forEach((btn) => {
+  btn.addEventListener('click', (event) => {
+	event.preventDefault()
+	const {target} = event
+	activeBtnClassAdder(target)
+	changeSlideByNav(slidesCount-target.getAttribute(`data-number`))
+  });
 });
 
-let buttonService = document.querySelector("#buttonService");
-buttonService.addEventListener("click", (event) => {
-  event.preventDefault();
-  console.log("click2");
-  changeSlideByNav(2);
-});
 
-let buttonExample = document.querySelector("#buttonExample");
-buttonExample.addEventListener("click", (event) => {
-  event.preventDefault();
-  console.log("click3");
-  changeSlideByNav(1);
-});
 
-let buttonContact = document.querySelector("#buttonContact");
-buttonContact.addEventListener("click", (event) => {
-  event.preventDefault();
-  console.log("click4");
-  changeSlideByNav(0);
-});
 
-let changeSlideByNav = (num) => {
+
+
+
+
+
+
+
+
+//add border to active button
+
+const changeSlideByNav = (num) => {
   activeSlideIndex = num;
   const height = container.clientHeight;
   mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
   sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
 };
+
+const mainLogo = document.querySelector('#mainLogo');
+mainLogo.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log('logo_clicked');
+});
+
+// //swipe for mobile
+// document.addEventListener('touchstart', handleTouchStart, false)
+// document.addEventListener('touchmove', handleTouchMove, false)
+
+// // let x1=null
+// let y1=null
+
+// function handleTouchStart(event) {
+// 	// const firstTouch = event.touches[0]
+// 	// x1 = firstTouch.clientX
+// 	// y1 = firstTouch.clientY
+// 	y1 = event.touches[0].clientY
+
+// }
+
+// function handleTouchMove(event) {
+// 	// console.log(event)
+// 	// if (!x1||!y1) {return false}
+// 	if (!y1) {return false}
+// 	// let x2=event.touches[0].clientX
+// 	let y2=event.touches[0].clientY
+// 	// console.log(x2,y2);
+// 	// let xDiff=x2-x1
+// 	let yDiff = y2-y1
+// 	// console.log(xDiff,yDiff);
+
+// 	// if (Math.abs(xDiff)>Math.abs(yDiff)){
+// 	// 	xDiff>0?console.log('right'):console.log('left');;
+// 	// } else {
+// 		yDiff>0?changeSlide('up'):changeSlide('down');;
+// 	// }
+// 	// console.log('y1 do', y1);
+// 	// x1=null
+// 	y1=null
+// 	// console.log('y1 posle', y1);
+// }
+
+// //()=>{console.log('touchstart')}
+// //()=>{console.log('touchmove')}
+
+////////////
+
+// //swipe for mobile
+
+// document.addEventListener('touchstart',handleDocumentTouchStart)
+// function handleDocumentTouchStart(event) {
+// 	// console.log(event.touches[0]);
+// 	console.log('x',event.touches[0].clientX);
+// 	console.log('sidebar', sidebar.clientWidth)
+// 	console.log('mainSlide', mainSlide.clientWidth)
+// 	event.touches[0].clientX>sidebar.clientWidth?console.log('clicked mainBar'):console.log('clicked sideBar');
+// }
+
+// sidebar.addEventListener('touchstart', handleTouchStart, false);
+// sidebar.addEventListener('touchmove', handleTouchMove, false);
+
+// // mainSlide.addEventListener('touchstart', handleTouchStart, false);
+// // mainSlide.addEventListener('touchmove', handleTouchMove, false);
+
+// let y1 = null;
+
+// function handleTouchStart(event) {
+//   y1 = event.touches[0].clientY;
+
+// //   console.log('y',event.touches[0].clientY);
+// }
+
+// function handleTouchMove(event) {
+//   if (!y1) {
+//     return false;
+//   }
+//   let y2 = event.touches[0].clientY;
+//   let yDiff = y2 - y1;
+
+//   if (yDiff < -5) {
+//     changeSlide('down');
+//   } else if (yDiff > 5) {
+//     changeSlide('up');
+//   }
+
+//   y1 = null;
+// }
